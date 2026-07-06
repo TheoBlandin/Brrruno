3a277c494712843cb0c7c604180f7de20df65408
Théo Blandin <feozard@gmail.com>
2026-07-06 10:58:10 +0200

    déplacer adresse du serveur dans .env + gitignore

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 # config.py
 
-SERVER = "irc.twelvegrimmauldplace.fr"
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv() 
+
+SERVER = os.getenv("SERVER")
 PORT = 6667
 
 NICK = "Brrruno"
